e6949a2dfbc0b8f919933b6bc1a68ddbb28be913
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-07 16:14:11 +0530

    variable type: null and undefined type

--- variable_type.ts
+++ variable_type.ts
@@ -10,3 +10,12 @@ console.log(isBegineer, total, name);
 let sentence : string = `My name is ${name}
 I am Begineer in TypeSript`;
 console.log(sentence);
+
+//null and undefined , not much use on their own
+let n : null = null;
+let u : undefined = undefined;
+
+//null and undefined used with number, boolean or string type
+let isNew : boolean = null;
+let myName: string = undefined;
+console.log(isNew, myName, n, u);
