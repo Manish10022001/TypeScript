6cd1fe1e58f5010e35e46d4d8f6336c1ca36b651
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-07 16:08:59 +0530

    Variable type : template literal

--- variable_type.ts
+++ variable_type.ts
@@ -5,3 +5,8 @@ let isBegineer : boolean = true;
 let total : number = 0;
 let name : string = "Manish"
 console.log(isBegineer, total, name);
+
+//template Literal
+let sentence : string = `My name is ${name}
+I am Begineer in TypeSript`;
+console.log(sentence);
