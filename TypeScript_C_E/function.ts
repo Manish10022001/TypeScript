6a4a4a661eba5322600eb4117b8d0c57707af23c
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-08 15:24:35 +0530

    Function with optional parameters

--- TypeScript_C_E/function.ts
+++ TypeScript_C_E/function.ts
@@ -5,3 +5,13 @@ function add(num1:number, num2:number): number{
 add(4,5);
 //add(4,"abc"); error
 console.log(add(4,5));
+
+//optional parameter
+function sub(num1:number, num2?:number){
+    if(num2)
+        return num1-num2
+    else
+        return num1;
+}
+console.log(sub(4,2));
+console.log(sub(3));
